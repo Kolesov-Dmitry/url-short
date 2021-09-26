d64e3a00c878660fb84f3250eea67e172cb703ab
diman <klsv.dmitry@gmail.com>
2021-09-26 11:39:43 +0700

    Simple settings loading from Environment variables

---
+++ internal/app/settings.go
@@ -0,0 +1,65 @@
+package app
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/spf13/viper"
+)
+
+const (
+	tagName        = "env"
+	settingsPrefix = "URL_SHORT"
+)
+
+// settings holds the application main settings
+type settings struct {
+	port int `env:"PORT"`
+}
+
+// loadSettings loads application main settings
+// Output:
+//    settings in case of success, otherwise returns error
+func loadSettings() (*settings, error) {
+	viper.SetEnvPrefix(settingsPrefix)
+	s := &settings{}
+
+	// bind values to viper
+	if err := s.forAll(func(tag string, isZero bool) error {
+		return viper.BindEnv(tag)
+	}); err != nil {
+		return nil, err
+	}
+
+	s.port = viper.GetInt("port")
+
+	// check if all parametres are set
+	if err := s.forAll(func(tag string, isZero bool) error {
+		if isZero {
+			return fmt.Errorf("environment value `%s_%s` should be set", settingsPrefix, tag)
+		}
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	return s, nil
+}
+
+// forAll applies provided function to all fields of settings struct
+// Inputs:
+//   f function than needs to be applied
+// Output:
+//   returns error if provided functions returns error
+func (s *settings) forAll(f func(tag string, isZero bool) error) error {
+	t := reflect.TypeOf(*s)
+	v := reflect.ValueOf(*s)
+	for i := 0; i < v.NumField(); i++ {
+		field := t.Field(i)
+		if err := f(field.Tag.Get(tagName), v.Field(i).IsZero()); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
