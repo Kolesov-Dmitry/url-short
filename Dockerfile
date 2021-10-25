9311fc4e0b71f222f8623cef3a2ac09ef0d47a2d
diman <klsv.dmitry@gmail.com>
2021-10-25 09:38:16 +0700

    Wrap the service into docker image

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+# Builder Stage
+FROM golang:1.17.2-alpine3.14 as builder
+
+# Copy application to the container
+COPY . /go/src/url-short
+
+# Get dependencies
+WORKDIR /go/src/url-short
+RUN go get -d -v ./...
+
+# Build the application
+WORKDIR /go/src/url-short/bin
+RUN go build -o ./url-short ../cmd/url-short/main.go
+
+# Run Stage
+FROM alpine:3.9
+
+COPY --from=builder /go/src/url-short/bin /app/url-short
+
+ENV URL_SHORT_PORT="8000"
+ENV URL_SHORT_DATABASE_URL="postgres://backend:qwer1234@localhost:5433/url_db"
+
+EXPOSE 8000
+
+# Run application
+WORKDIR /app/url-short
+CMD ["./url-short"]
---
+++ deploy/postgres/schema.sql
@@ -0,0 +1,31 @@
+-- create database
+CREATE DATABASE url_db;
+\c url_db
+
+-- creates user
+CREATE USER backend WITH PASSWORD 'qwer1234';
+GRANT CONNECT ON DATABASE blog TO backend;
+
+-- create schema for urls tables
+CREATE SCHEMA urls;
+
+-- create urls tables
+CREATE TABLE IF NOT EXISTS urls.urls (
+  "url_hash" VARCHAR(10) NOT NULL,
+  "url" TEXT NOT NULL,
+  PRIMARY KEY ("url_hash")
+);
+
+CREATE TABLE IF NOT EXISTS urls.urls_linking (
+	"id" UUID NOT NULL,
+	"url_hash" VARCHAR(10) NOT NULL,
+	"visited_at" TIMESTAMP NOT NULL DEFAULT 'CURRENT_TIMESTAMP',
+	PRIMARY KEY ("id"),
+	CONSTRAINT "FK_urls_linking_urls" FOREIGN KEY ("url_hash") REFERENCES urls.urls ("url_hash") ON UPDATE NO ACTION ON DELETE CASCADE
+);
+
+-- grant privilegies to the user
+GRANT usage ON SCHEMA urls TO backend;
+GRANT select, insert, update, delete, trigger ON all tables IN SCHEMA urls TO backend;
+GRANT usage, select ON all sequences IN SCHEMA urls TO backend;
+GRANT execute ON all functions IN SCHEMA urls TO backend;
